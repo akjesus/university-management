57731978e1e819525030dd30bda44b59dab79274
Otong Akan <cutytongy@gmail.com>
2025-05-28 20:11:23 +0100

    renamed frontend folder

---
+++ Frontend/src/utility/toasts.js
@@ -0,0 +1,19 @@
+const commonToastAttributes = {
+  position: "top-right",
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "dark",
+};
+
+export const toastSuccessObject = {
+  autoClose: 2000,
+  ...commonToastAttributes,
+};
+
+export const toastErrorObject = {
+  autoClose: 5000,
+  ...commonToastAttributes,
+};
